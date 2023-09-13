e17bc0c4760dd695a22bb370ff9cc32222af1b03
Onlyyoubabe <22610383@utgz.edu.mx>
2023-09-13 09:09:35 -0600

    13/09/23, Se realizo el ejercicio de Notas

---
+++ Practica2/src/main/java/com/mycompany/practica2/Practica2.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica2;
+
+/**
+ *
+ * @author Admin
+ */
+public class Practica2 {
+
+    public static void main(String[] args) {
+
+        Acta acta = new Acta(3,4);
+        acta.poblar();
+        
+        for (int i=0; i < acta.getNotas().length; i++){
+            for(int j = 0; j < acta.getNotas()[0].length; j++){
+                System.out.println(acta.getNotas()[i][j] + "-");
+            }
+            System.out.println("");
+        }
+    }
+}
